db80d10d5cfb84b91c52babcf57db2aa5bfbe1b7
Chen Wei <weichen@apache.org>
2023-10-08 10:16:55 +0800

    2023-10-08-remove-ol3dc.js

--- build/build.sh
+++ build/build.sh
@@ -8,9 +8,5 @@ npx js-beautify -r 'js/**/*.js'
 npx js-beautify -r '*/worker.js'
 npx js-beautify -r '*/single.js'
 
-echo \> Combine JS Export Files: ol3dc.js
-mkdir -p build
-find js -type f -name "*.js" | xargs cat > build/ol3dc.js
-
 runtime=$(echo "scale=9;($(date +%s%N) - ${basetime})/(1*10^09)" | bc)
 echo \> Build Duration: $runtime Seconds

b7834af304971edba165f42181bee1e720bf59c6
Sharad Mayank Sinha <ghostkwebb@gmail.com>
2025-12-13 18:05:20 +0530

    fix: address PR feedback (default false, push logic)

--- js/ml/ml-manager.js
+++ js/ml/ml-manager.js
@@ -115,14 +115,14 @@ async function onHolisticResults(results) {
 
     postCoreLog(allLog);
     postPoI(allPoI);
-    if (results.faceLandmarks || getCMV('USE_IFACIALMOCAP')) {
+    if (results.faceLandmarks && !getCMV('USE_IFACIALMOCAP')) {
         pushInfo(tmpInfo);
     }
 }
 
 window.applyIFacialMocapData = function (data) {
     let useMocap = getCMV('USE_IFACIALMOCAP');
-    if (useMocap === undefined) useMocap = true;
+    if (useMocap === undefined) useMocap = false;
 
     if (!useMocap) return;
 

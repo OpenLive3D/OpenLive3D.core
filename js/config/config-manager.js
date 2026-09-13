68a82df305be0d2e916060035f090d6b6f066574
Wei-1 <Wei-1@users.noreply.github.com>
2026-09-13 13:33:23 +0000

    deploy: d3b92723bae39873dec67b14ce19f2d360de373f

--- js/config/config-manager.js
+++ js/config/config-manager.js
@@ -1,6 +1,6 @@
 // version configuration
-const DEV_DATE = "2026-07-11";
-const VERSION = "v.2.6.2";
+const DEV_DATE = "2026-09-13";
+const VERSION = "v.2.6.3";
 const CONFIG_VERSION = "Beta.1.2.12";
 
 let configManager = {};
